fa1a72f98885613d1655e84ac57a62c9c6e329a7
Unknown <c_bron02@uni-muenster.de>
2018-11-27 14:44:57 +0100

    shapefile function added

--- Metadatenextraktion/Metadataextraction.py
+++ Metadatenextraktion/Metadataextraction.py
@@ -1,31 +1,26 @@
 from osgeo import gdal
 import pandas as pd
 import pygeoj
-def firsttry(path):
+import shapefile
+
+def getMetaData(path):
 
 """returns the metadata of supported Datatypes. supported data: Shapefile (.shp), 
     GeoJson (.json/.geojson), GeoTIFF (.tif), netCDF (.nc), GeoPackage (.gpkg) CSV on the web
     @param path to the file """
+
     # connect path to file
      filepath = "%s\%s" % (path, name)p
       
     switcher = { """ filepath immer in (" ") eingeben """
-        1: gdalinfo(filepath),              """GeoTiff""" 
+        1: gdalinfo(filepath),              """GeoTiff getGeoTiffBoundingBox(name, path) """ 
         2: pd.read_csv(filepath),           """CSV"""
         3: (test).bbox                      """GeoJson; bounding box of entire file: test=pygeoj.load(filepath)""""
-        4: shapefile.Reader(filepath)       """Shapefile"
+        4: shapefile.Reader(filepath)       """Shapefile"""
         5:
         6:
     }
     print switcher.get(path, click.echo("type %s not supported" % filepath))
-
- """ if (1+1=2):
-  try:
-      gdalinfo(filepath)
-      ogrinfo(filepath)
-else: 
-     click.echo("type %s not supported" % filepath)
-        return None"""
 
 """
 Main Methode anpassen
