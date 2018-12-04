52a35d2e6567ece5fc62941964e54b5f43296579
Unknown <c_bron02@uni-muenster.de>
2018-12-04 11:17:09 +0100

    pip install added

--- yaml_install_Dateien.py
+++ yaml_install_Dateien.py
@@ -1,6 +1,12 @@
+pip install click
 pip install pyshp
+pip install shapefile
 pip install pandas
 pip install PyGeoj
 pip install numpy
 pip install pandas
 pip install xarray
+pip install sqlite3
+pip install csv
+
+pip install gdal
