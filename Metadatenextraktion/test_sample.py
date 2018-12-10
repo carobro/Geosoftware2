1b3c7f3d1a115f26a387dc581592f61a5036f0c1
Unknown <c_bron02@uni-muenster.de>
2018-12-10 15:24:03 +0100

    kleinigkeiten

--- Metadatenextraktion/test_sample.py
+++ Metadatenextraktion/test_sample.py
@@ -7,7 +7,7 @@ import xarray as xr
 import detailebenen
 
 def test_answer():
-    assert detailebenen.getShapefilebbx('/home/paulsenmann/Documents/gs2/testdata/shapefile/Abgrabungen_Kreis_Kleve/Abgrabungen_Kreis_Kleve_Shape.shp', 'bbx') == 'bbx'
+    assert detailebenen.getShapefilebbx('/home/paulsenmann/Documents/gs2/testdata/shapefile/Abgrabungen_Kreis_Kleve/Abgrabungen_Kreis_Kleve_Shape.shp', 'bbx') == "295896.274870878, 5694747.64703736, 325999.79578122497, 5747140.98659967"
 
 def test_answer1():    
     assert detailebenen.getCSVbbx('/home/paulsenmann/Documents/gs2/testdata/Behindertenparkplaetze_Duesseldorf.csv', 'bbx') == 42
