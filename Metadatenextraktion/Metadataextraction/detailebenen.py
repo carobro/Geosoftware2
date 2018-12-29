0516cbe19ee5f809021b97bfec6b796d31112a65
CorneliaZy <c_zyga01@uni-muenster.de>
2018-12-29 15:19:00 +0100

    added help function for CLI tool

--- Metadatenextraktion/Metadataextraction/detailebenen.py
+++ Metadatenextraktion/Metadataextraction/detailebenen.py
@@ -15,12 +15,12 @@ bboxSpeicher = []
 """ Vorteil uneres Codes: Es wird nicht auf die Endung (.shp etc.) geachtet,
 sondern auf den Inhalt"""
 @click.command()
-@click.option('--path',required=True, help='Path to the data.')
+@click.option('--path',required=True, help='please insert the path to the data here.')
 @click.option('--bbox', 'detail', flag_value='bbox',
-              default=True)
-@click.option('--feature', 'detail', flag_value='feature')
-@click.option('--single', 'folder', flag_value='single', default=True)
-@click.option('--whole', 'folder', flag_value='whole')
+              default=True, help='returns the extent of an object as a boundingbox')
+@click.option('--feature', 'detail', flag_value='feature', help='returns a more detailed representation of the extent of one object.')
+@click.option('--single', 'folder', flag_value='single', default=True, help='returns all the boundingboxes from objects of a folder')
+@click.option('--whole', 'folder', flag_value='whole', help='returns one overall boundingbox from all objects of a folder')
 
 
 
