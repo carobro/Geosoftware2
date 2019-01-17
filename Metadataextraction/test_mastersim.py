c539c8b1a44be6c6abda17f784287645a621c6c9
Unknown <c_bron02@uni-muenster.de>
2019-01-17 18:33:19 +0100

    Hauptfunktion
    Hauptfunktion die alle Funktionen zur Berechnung des Sim Scores aufruft (extractTool.py, similar.py) + Vorlage für Tests (werden noch ergänzt)

---
+++ Metadataextraction/test_mastersim.py
@@ -0,0 +1,67 @@
+import click, shapefile, json, sqlite3, csv, pygeoj
+from osgeo import gdal
+import pandas as pd
+import numpy as np
+import xarray as xr
+import os
+
+import extractTool, mastersim, similar
+import getShapefileInfo, getGeoTiffInfo, getCSVInfo, getGeoJsonInfo, getNetCDFInfo, getGeoPackageInfo, getIsoInfo, openFolder
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+"""
+These tests check if the calucated similar score from two files is
+equal to our hand-calculted score
+"""
+
+def test_master1():
+    filepath1 = ""
+    filepath2 = ""
+    assert mastersim.master(filepath1, filepath2) == ""
+
+'''
+def test_master2():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master3():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master4():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master5():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master6():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master7():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master8():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master9():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+
+def test_master10():
+    filepath1 = 
+    filepath2 = 
+    assert mastersim.master(filepath1, filepath2) == 
+'''
