0eaeb421ce6b61db17c5b808b6323a5be5443ce5
Unknown <c_bron02@uni-muenster.de>
2019-01-23 11:38:59 +0100

    tests for folder

--- Metadataextraction/test_whole.py
+++ Metadataextraction/test_whole.py
@@ -9,30 +9,35 @@ import dateparser
 import extractTool
 import getShapefileInfo, getGeoTiffInfo, getCSVInfo, getGeoJsonInfo, getNetCDFInfo, getGeoPackageInfo, getIsoInfo, openFolder
 
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 ###############################
 # --detail=bbox --folder=whole
 ###############################
 def test_wholeA():
-    filepath='https://uni-muenster.sciebo.de/s/QFj5pzm7AzxAh1f?path=%2FfunktionierendeTestdaten_gruppe1%2Fmischung'
-    filepath1="/home/minicaro/Downloads/mischung"
-    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == None
+    filepath=__location__+'/testdata/mischung_bbox1'
+    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == [[-43.7405, 50.31025197410836, 9.468398712484145, 167.998], [None], [None]]
+
+def test_wholeD():  
+    filepath=__location__+"/testdata/mischung_bbox2"  
+    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == [[5.520648869321924, 49.87014441103477, 10.114607987362609, 52.88446415203189], [None], [None]]
 
 def test_wholeB():    
-    filepath='https://uni-muenster.sciebo.de/s/QFj5pzm7AzxAh1f?path=%2FfunktionierendeTestdaten_gruppe1%2Fgeotiff'
-    filepath1="/home/minicaro/Downloads/geotiff"
-    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == None
+    filepath=__location__+'/testdata/geotifftest'
+    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == [[5.520648869321924, 49.87014441103477, 10.114607987362609, 52.88446415203189], [None], [None]]
 
 def test_wholeC():  
-    filepath="https://uni-muenster.sciebo.de/s/QFj5pzm7AzxAh1f?path=%2FfunktionierendeTestdaten_gruppe1%2Fgeopackage"  
-    filepath1="/home/minicaro/Downloads/geopackage"
-    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == None
+    filepath=__location__+"/testdata/geopackagetest"  
+    assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == [[-43.7405, 96.8169, -9.14218, 167.998], [None], [None]]
 
+
+"""
 #####################################
 # --detail=bbox --folder=whole --time
 #####################################
 def test_wholeD():
     filepath="https://uni-muenster.sciebo.de/s/QFj5pzm7AzxAh1f?path=%2FfunktionierendeTestdaten_gruppe1%2Fzeitdaten%2Ftimegeo"
-    filepath1="/home/minicaro/Downloads/timegeo"
+    filepath1="/home/minicaro/Downloads/timegeo/timegeo"
     assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == None
 
 def test_wholeE():    
@@ -40,3 +45,4 @@ def test_wholeE():
     filepath1="/home/minicaro/Downloads/time_mischung"
     assert extractTool.getMetadata(filepath, 'bbox', 'whole', False) == None
 
+"""
